0e5875c56488ceb34a7173efc5f5a11b9d8e46f7
FastRabbitON <fastrabbiton@gmail.com>
2023-09-01 18:02:18 +0200

    MainCode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import LegalFLY from './LegalFLY';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <App />
+  <LegalFLY />
 
 );
 
